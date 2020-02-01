a125c16ff6faf27d4abad47807583023b2dadf19
Théotime COLIN <contact@theotimecolin.com>
2020-02-01 14:50:15 +0100

    feat(Setup): setup general hierarchy and assets

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <Homepage class="Homepage" />
+  </div>
+</template>
+
+<script>
+import Homepage from './pages/Homepage.vue'
+
+export default {
+  name: 'app',
+  components: { Homepage }
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/scss/index.scss";
+</style>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Homepage">
+        <section class="bg-candy">
+            <div class="Wrapper  pv-150">
+                <div class="row">
+                    <div class="col-8">
+                        <h1 class="ft-2xl">
+                            <span class="color-sugar ft-xl">✌️ <b>antiswipe c'est</b></span><br> 
+                            <b>des sorties sur Paris entre jeunes lgbt+ qui en ont <b>marre des applis de rencontres.</b></b>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Homepage'
+}
+</script>
